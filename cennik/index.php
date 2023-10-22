cdf8d041e5aa831f4036f1ce6ef18b9fce064813
novy213 <j.solarek@wp.pl>
2023-10-22 19:28:06 +0200

    fixed layout

--- cennik/index.php
+++ cennik/index.php
@@ -8,115 +8,111 @@
     <link rel="icon" href="Maciej_Woloszyn-01-transparent.jpg">
 </head>
 <body>
-<header>
-    <h1>Cennik</h1>
-</header>
-<div id="cennik" class="minimalistyczny">
-    <table>
-        <tr>
-            <th>Nazwa</th>
-            <th>Cena</th>
-        </tr>
-        <tr>
-            <td>1TP</td>
-            <td>140zł</td>
-        </tr>
-        <tr>
-            <td>4TP</td>
-            <td>440zł</td>
-        </tr>
-        <tr>
-            <td>8TP</td>
-            <td>800zł</td>
-        </tr>
-        <tr>
-            <td>12TP</td>
-            <td>1100zł</td>
-        </tr>
-        <tr>
-            <td>20TP</td>
-            <td>1800zł</td>
-        </tr>
-        <tr>
-            <td>40TP</td>
-            <td>3500zł</td>
-        </tr>
-        <tr>
-            <td>1TP DUET</td>
-            <td>200zł</td>
-        </tr>
-        <tr>
-            <td>4TP DUET</td>
-            <td>600zł</td>
-        </tr>
-        <tr>
-            <td>8TP DUET</td>
-            <td>1100zł</td>
-        </tr>
-        <tr>
-            <td>12TP DUET</td>
-            <td>1500zł</td>
-        </tr>
-        <tr>
-            <td>20TP DUET</td>
-            <td>2400zł</td>
-        </tr>
-        <tr>
-            <td>40TP DUET</td>
-            <td>4000zł</td>
-        </tr>
-        <tr>
-            <td>Trening wprowadzający</td>
-            <td>0zł</td>
-        </tr>
-    </table>
-</div>
-<div class="container minimalistyczny">
-    <h1>Zapisz się na Trening Personalny</h1>
-    <form method="POST">
-        <label for="imie">Imię:</label>
-        <input type="text" id="imie" name="imie" required placeholder="Wprowadź swoje imię">
+<div class="container">
+    <header>
+        <h1>Cennik</h1>
+    </header>
+    <div id="cennik" class="minimalistyczny">
+        <table>
+            <tr>
+                <th>Nazwa</th>
+                <th>Cena</th>
+            </tr>
+            <tr>
+                <td>Trening wprowadzający</td>
+                <td>0zł</td>
+            </tr>
+            <tr>
+                <td>1TP</td>
+                <td>140zł</td>
+            </tr>
+            <tr>
+                <td>4TP</td>
+                <td>440zł</td>
+            </tr>
+            <tr>
+                <td>8TP</td>
+                <td>800zł</td>
+            </tr>
+            <tr>
+                <td>12TP</td>
+                <td>1100zł</td>
+            </tr>
+            <tr>
+                <td>20TP</td>
+                <td>1800zł</td>
+            </tr>
+            <tr>
+                <td>40TP</td>
+                <td>3500zł</td>
+            </tr>
+            <tr>
+                <td>1TP DUET</td>
+                <td>200zł</td>
+            </tr>
+            <tr>
+                <td>4TP DUET</td>
+                <td>600zł</td>
+            </tr>
+            <tr>
+                <td>8TP DUET</td>
+                <td>1100zł</td>
+            </tr>
+            <tr>
+                <td>12TP DUET</td>
+                <td>1500zł</td>
+            </tr>
+            <tr>
+                <td>20TP DUET</td>
+                <td>2400zł</td>
+            </tr>
+            <tr>
+                <td>40TP DUET</td>
+                <td>4000zł</td>
+            </tr>
+        </table>
+    </div>
+    <div class="form">
+        <h1>Zapisz się na Trening Personalny</h1>
+        <form method="POST">
+            <label for="imie">Imię:</label>
+            <input class="inputy" type="text" id="imie" name="imie" required placeholder="Wprowadź swoje imię">
 
-        <label for="email">Adres e-mail:</label>
-        <input type="email" id="email" name="email" required placeholder="Wprowadź swój adres e-mail">
+            <label for="email">Adres e-mail:</label>
+            <input class="inputy" type="email" id="email" name="email" required placeholder="Wprowadź swój adres e-mail">
 
-        <label for="telefon">Telefon:</label>
-        <input type="tel" id="telefon" name="telefon" required placeholder="Wprowadź swój numer telefonu">
+            <label for="trener">Temat:</label>
+            <input class="inputy" type="text" id="trener" name="trener" required placeholder="Wprowadź temat">
 
-        <label for="trener">Temat:</label>
-        <select id="trener" name="trener">
-            <option value="trening">Trening personalny</option>
-        </select>
+            <label for="wiadomosc">Dodatkowe informacje:</label><br>
+            <textarea class="inputy" id="wiadomosc" name="wiadomosc" rows="4"></textarea><br>
+            <input type="submit" value="Zapisz się" class="submit">
+        </form>
+        <?php
+        if(isset($_POST['submit'])) {
+            $imie = $_POST["imie"];
+            $email = $_POST["email"];
+            $telefon = $_POST["telefon"];
+            $trener = $_POST["trener"];
+            $wiadomosc = $_POST["wiadomosc"];
 
-        <label for="wiadomosc">Dodatkowe informacje:</label><br>
-        <textarea id="wiadomosc" name="wiadomosc" rows="4"></textarea><br>
-        <input type="submit" name="submit" value="Zapisz się">
-    </form>
-    <?php
-    if(isset($_POST['submit'])) {
-        $imie = $_POST["imie"];
-        $email = $_POST["email"];
-        $telefon = $_POST["telefon"];
-        $trener = $_POST["trener"];
-        $wiadomosc = $_POST["wiadomosc"];
+            $subject = "Nowa wiadomość z formularza";
+            $to = "maciejwoloszyn04@gmail.com";
 
-        $subject = "Nowa wiadomość z formularza";
-        $to = "maciejwoloszyn04@gmail.com";
+            $message = "Imię: $imie\n <br>";
+            $message .= "Email: $email\n <br>";
+            $message .= "Telefon: $telefon\n <br>";
+            $message .= "Temat: $trener\n <br>";
+            $message .= "Wiadomość:\n$wiadomosc <br>";
 
-        $message = "Imię: $imie\n <br>";
-        $message .= "Email: $email\n <br>";
-        $message .= "Telefon: $telefon\n <br>";
-        $message .= "Temat: $trener\n <br>";
-        $message .= "Wiadomość:\n$wiadomosc <br>";
+            $headers = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-Type: text/html; charset=utf-8' . "\r\n";
 
-        $headers = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-Type: text/html; charset=utf-8' . "\r\n";
-
-        mail($to, $subject, $message, $headers);
-    }
-    ?>
-</div>
-<footer class="minimalistyczny">
+            mail($to, $subject, $message, $headers);
+        }
+        ?>
+    </div>
+<footer>
     <div class="stopka">
         <div class="logo">
             <img src="Maciej_Woloszyn-01-transparent.jpg" alt="Logo" style="width: 75%;">
@@ -127,5 +123,6 @@
         </div>
     </div>
 </footer>
+</div>
 </body>
 </html>
